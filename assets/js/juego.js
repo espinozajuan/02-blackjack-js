66e992131a29d471110357f45baaf89736bf5f34
Juan Gabriel Espinoza <50055314+espinozajuan@users.noreply.github.com>
2021-12-16 09:23:21 -0300

    Cartas js

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -7,6 +7,7 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+//Esta func crea un nuevo deck
 const crearDeck = () => {
 
     for (let i = 2; i <= 10; i++) {
@@ -21,7 +22,6 @@ const crearDeck = () => {
         }
     }
 
-    console.log( deck );
     deck = _.shuffle( deck );
     console.log( deck );
     return deck;
@@ -29,3 +29,30 @@ const crearDeck = () => {
 }
 
 crearDeck();
+
+//Esta func permite tomar una carta
+const pedirCarta = () => {
+
+    if ( deck.length === 0 ){
+        throw 'No hay cartas en el deck';
+    }
+
+    const carta = deck.pop();
+    
+    console.log(deck);
+    console.log(carta); //carta debe ser del deck
+    return carta;
+}
+
+// pedirCarta();
+const valorCarta = ( carta ) => {
+
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN( valor ) ) ?
+            ( valor === 'A' ) ? 11 : 10
+            : valor * 1;  
+
+}
+
+const valor = valorCarta( pedirCarta() );
+console.log( valor );
